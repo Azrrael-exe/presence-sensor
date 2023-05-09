936ca7835ab3913a63e0ab49a943d9af5ddadceb
Victor Camargo <victor.camargo@rappi.com>
2023-05-09 18:07:13 -0500

    On branch feature/parking_id Changes to be committed: 	modified:   src/config/constants.h 	modified:   src/main.cpp 	new file:   src/parking/parking.h
    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
-#include <Adafruit_NeoPixel.h>
 #include <ArduinoJson.h>
 
-#include "sensors/pir_sensor.h"
+#include "sensors/dwarf_sensor.h"
 #include "config/constants.h"
 #include "com/wifi.h"
 #include "com/mqtt.h"
-
+#include "parking/parking.h"
 
 String input_buffer;
 bool input_queue = false;
@@ -21,9 +20,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(input_buffer);
 }
 
-Adafruit_NeoPixel pixels(LIGHTS_COUNT, LIGHTS_PIN, NEO_GRB + NEO_KHZ800);
+Parking parking = Parking(0x0ABC, 20);
 
-PirSensor sensor = PirSensor(SENSOR_PIN);
+DwarfSensor sensor = DwarfSensor(SENSOR_PIN, 2, 5000);
 WiFiClient espClient;
 WiFiManager manager;
 PubSubClient mqtt_client(MQTT_BROKER, 1883, callback, espClient);
@@ -33,9 +32,6 @@ MqttManager mqtt_manager = MqttManager(MQTT_CLIENT_ID, MQTT_IN_TOPIC, MQTT_OUT_T
 void setup() {
   sensor.config();
   Serial.begin(115200);
-  pixels.begin();
-  pixels.fill(pixels.Color(0, 0, 0), 0, LIGHTS_COUNT);
-  pixels.clear();
   WiFi.begin(SSID, SSID_PASSWORD);
   pinMode(LIGHTS_PIN, OUTPUT);
   manager.connect(WiFi, Serial);
@@ -46,27 +42,5 @@ void loop() {
   sensor.loop();
   mqtt_client.loop();
   manager.checkStatus(WiFi, Serial);
-
-  if(input_queue) {
-    DynamicJsonDocument input(200);
-    DeserializationError error = deserializeJson(input, input_buffer);
-
-    if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.f_str());
-    }
-    
-    pixels.fill(pixels.Color(input["red"], input["green"], input["blue"]), 0, LIGHTS_COUNT);
-    pixels.show();
-    input_queue = false;
-  }
-
-  if(sensor.hasChanged()){
-    Serial.println(sensor.getValue());
-    DynamicJsonDocument  output(20);
-    output["sensor"] = sensor.getValue();
-    String output_str;
-    serializeJson(output, output_str);
-    mqtt_client.publish(MQTT_OUT_TOPIC, output_str.c_str());
-  }
+  parking.loop(sensor, mqtt_client);
 }
---
+++ src/sensors/dwarf_sensor.h
@@ -0,0 +1,71 @@
+#ifndef DWARF_SENSOR_H
+#define DWARF_SENSOR_H
+
+#include <Arduino.h>
+#include "sensors/sensor.h"
+
+class DwarfSensor:public SensorABC {
+    private:
+        int inPin;
+        int outPin;
+        int read();
+        long timer;
+        long threshold;
+        int value;
+        bool changed;
+    public:
+        bool hasChanged();
+        void config();
+        void loop();
+        bool getValue();
+        DwarfSensor(int inPin, int outPin, long threshold);   
+};
+
+DwarfSensor::DwarfSensor(int inPin, int outPin, long threshold) {
+    this->inPin = inPin;
+    this->outPin = outPin;
+    this->timer = 0;
+    this->threshold = threshold;
+}
+
+int DwarfSensor::read() {
+    if (digitalRead(this->inPin)){
+        return 1;
+    } else if (digitalRead(this->outPin)) {
+        return -1;
+    } else {
+        return 0;
+    }
+    
+}
+
+void DwarfSensor::loop() {
+    int actual_value = this->read();
+    if (actual_value != 0) {
+        if ((this->value != actual_value) || (millis() - this->timer >= this->threshold)) {
+            this->timer = millis();
+            this->changed = true;
+            this->value = actual_value;
+            Serial.println(this->hasChanged());
+        }
+    }
+}
+
+bool DwarfSensor::getValue() {
+    this->changed = false;
+    if(this->value == 1) {
+        return true;
+    }
+    return false;
+}
+
+void DwarfSensor::config() {
+    pinMode(this->inPin, INPUT);
+    pinMode(this->outPin, INPUT);
+}
+
+bool DwarfSensor::hasChanged(){
+    return this->changed;
+}
+
+#endif
